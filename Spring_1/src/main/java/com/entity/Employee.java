96adb99149c064c11d7496f8f27a4fed2538743c
Mahesh Shelar <mshelar334@gmail.com>
2025-10-09 22:57:00 +0530

    first commit

---
+++ Spring_1/src/main/java/com/entity/Employee.java
@@ -0,0 +1,49 @@
+package com.entity;
+
+
+
+public class Employee {
+
+
+	int emp_id;
+	String emp_name;
+	String emp_salary;
+	
+	
+	
+	
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	public String getEmp_salary() {
+		return emp_salary;
+	}
+	public void setEmp_salary(String emp_salary) {
+		this.emp_salary = emp_salary;
+	}
+	public Employee(int emp_id, String emp_name, String emp_salary) {
+		super();
+		this.emp_id = emp_id;
+		this.emp_name = emp_name;
+		this.emp_salary = emp_salary;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Employee [emp_id=" + emp_id + ", emp_name=" + emp_name + ", emp_salary=" + emp_salary + "]";
+	}
+	
+	
+}
